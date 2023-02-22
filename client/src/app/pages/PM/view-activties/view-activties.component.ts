2136bd5c856ea3d1b0b89ee1be40c9196f201bb7
kirubel gizaw <kirub.hel@gmail.com>
2023-02-22 09:19:32 +0300

    actview

---
+++ client/src/app/pages/PM/view-activties/view-activties.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-activties',
+  templateUrl: './view-activties.component.html',
+  styleUrls: ['./view-activties.component.css']
+})
+export class ViewActivtiesComponent {
+
+}
