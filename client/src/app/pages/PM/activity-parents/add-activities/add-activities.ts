771f7578044e3ec5c6d4114bf674a7473fe76281
kirubel gizaw <kirub.hel@gmail.com>
2023-02-22 09:59:40 +0300

    encodecase

---
+++ client/src/app/pages/PM/activity-parents/add-activities/add-activities.ts
@@ -0,0 +1,41 @@
+export interface AddActivitesDto {
+
+    ActivityDescription:string,
+    HasActivity:boolean,
+    TaskId:string,
+    CreatedBy:string
+
+}
+
+export interface ActivityDetailsDto{
+
+    
+}
+
+    {
+        "ActivityDescription": "string",
+        "HasActivity": true,
+        "TaskId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "CreatedBy": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "ActivityDetails": [
+          {
+            "SubActivityDesctiption": "string",
+            "StartDate": "string",
+            "EndDate": "string",
+            "PlannedBudget": 0,
+            "Weight": 0,
+            "ActivityType": 0,
+            "OfficeWork": 0,
+            "FieldWork": 0,
+            "UnitOfMeasurement": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "PreviousPerformance": 0,
+            "Goal": 0,
+            "TeamId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "CommiteeId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "Employees": [
+              "string"
+            ]
+          }
+        ]
+      }
+}
