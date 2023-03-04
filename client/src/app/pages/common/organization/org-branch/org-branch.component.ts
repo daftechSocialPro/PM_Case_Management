4e597deae0336c5ea5dc3b427ffdb067350f87da
ka1ea6 <kaleab77gulilat@gmail.com>
2023-03-04 12:55:52 +0300

    merge

--- client/src/app/pages/common/organization/org-branch/org-branch.component.ts
+++ client/src/app/pages/common/organization/org-branch/org-branch.component.ts
@@ -10,32 +10,35 @@ import { UpdateBranchComponent } from './update-branch/update-branch.component';
 @Component({
   selector: 'app-org-branch',
   templateUrl: './org-branch.component.html',
-  styleUrls: ['./org-branch.component.css']
+  styleUrls: ['./org-branch.component.css'],
 })
 export class OrgBranchComponent implements OnInit {
-
   branches: OrganizationBranch[] = [];
   toast!: toastPayload;
   branch!: OrganizationBranch;
 
-  constructor(private elementRef: ElementRef, private orgService: OrganizationService, private commonService: CommonService, private modalService: NgbModal) {
-    var s = document.createElement("script");
-    s.type = "text/javascript";
-    s.src = "../assets/js/main.js";
+  constructor(
+    private elementRef: ElementRef,
+    private orgService: OrganizationService,
+    private commonService: CommonService,
+    private modalService: NgbModal
+  ) {
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.src = '../assets/js/main.js';
     this.elementRef.nativeElement.appendChild(s);
   }
 
   ngOnInit(): void {
-    this.branchList()
+    this.branchList();
   }
 
   branchList() {
     this.orgService.getOrgBranches().subscribe({
       next: (res) => {
-        this.branches = res
-
-
-      }, error: (err) => {
+        this.branches = res;
+      },
+      error: (err) => {
         this.toast = {
           message: 'Something went wrong',
           title: 'Network error.',
@@ -46,42 +49,29 @@ export class OrgBranchComponent implements OnInit {
           } as IndividualConfig,
         };
         this.commonService.showToast(this.toast);
-      }
-    })
+      },
+    });
   }
 
   addBranch() {
-
     let modalRef = this.modalService.open(AddBranchComponent, {
       size: 'lg',
-      backdrop: 'static'
-    })
+      backdrop: 'static',
+    });
 
-    modalRef.result.then((result)=>{
-   
-        this.branchList;
-      
-      
-    })
-
-
-
+    modalRef.result.then((result) => {
+      this.branchList;
+    });
   }
 
   Update(vlaue: any) {
-
     let modalref = this.modalService.open(UpdateBranchComponent, {
       size: 'lg',
-      backdrop: 'static'
-    })
-    modalref.componentInstance.branch = vlaue
+      backdrop: 'static',
+    });
+    modalref.componentInstance.branch = vlaue;
     modalref.result.then((isresult) => {
-
-      this.branchList()
-
-    })
-
-
+      this.branchList();
+    });
   }
-
 }
